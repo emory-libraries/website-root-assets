a0920bfac14bb5ab01328a79df4052e0e93223c7
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-17 09:41:21 -0400

    Adds phplint grunt task to check `index.php` files for errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,19 @@ module.exports = (grunt) => {
   const path = require('path');
   
   // Configure tasks.
-  grunt.initConfig({});
+  grunt.initConfig({
+    phplint: {
+      all: ['**/index.php']
+    }
+  });
   
   // Load tasks.
   grunt.loadNpmTasks('grunt-plop');
+  grunt.loadNpmTasks('grunt-phplint');
   
   // Register tasks.
   grunt.registerTask('default', ['plop']);
+  grunt.registerTask('lint', ['phplint']);
   grunt.registerTask('deploy', require(path.resolve('scripts/deploy.js')));
   
 };

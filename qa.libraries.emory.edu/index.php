89140c6d8e78b89e781fe94f2da605cf6258aca9
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-18 09:08:39 -0400

    Updates the site indexes to include a map identifying environment folder names

--- qa.libraries.emory.edu/index.php
+++ qa.libraries.emory.edu/index.php
@@ -5,15 +5,23 @@ define('DOMAIN', 'qa.libraries.emory.edu');
 define('ENVIRONMENT', 'qa');
 define('SITE', 'libraries.emory.edu');
 
+// Set environment directories.
+$environment = [
+  'development' => 'dev',
+  'qa'          => 'qa',
+  'staging'     => 'staging',
+  'production'  => 'prod'
+];
+
 // Set path globals.
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('SITE_ROOT', __DIR__);
 define('SERVER_ROOT', dirname(__DIR__)); 
 define('SERVER_PATH', str_replace(DOCUMENT_ROOT.'/', '', SERVER_ROOT));
-define('DATA_ROOT', SERVER_ROOT.'/data/'.ENVIRONMENT);
-define('PATTERNS_ROOT', SERVER_ROOT.'/patterns/'.ENVIRONMENT);
-define('ENGINE_ROOT', SERVER_ROOT.'/engine/'.ENVIRONMENT);
-define('CACHE_ROOT', SERVER_ROOT.'/engine/'.ENVIRONMENT.'/php/cache');
+define('DATA_ROOT', SERVER_ROOT.'/data/'.$environment[ENVIRONMENT]);
+define('PATTERNS_ROOT', SERVER_ROOT.'/patterns/'.$environment[ENVIRONMENT]);
+define('ENGINE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT]);
+define('CACHE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT].'/php/cache');
 define('SITE_DATA', DATA_ROOT.'/'.SITE);
   
 // Load the templating engine.

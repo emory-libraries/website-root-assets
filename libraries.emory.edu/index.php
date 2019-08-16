eddac51309a8c1eaf853bf18feb409b17ab3db83
Lauren Hamel <lauren.hamel@outlook.com>
2019-08-16 10:05:15 -0400

    Updates `index.php` to include a map of folder names/locations based on environment

--- libraries.emory.edu/index.php
+++ libraries.emory.edu/index.php
@@ -5,25 +5,45 @@ define('DOMAIN', 'libraries.emory.edu');
 define('ENVIRONMENT', 'production');
 define('SITE', 'libraries.emory.edu');
 
-// Set environment directories.
+// Configure environment directories for data, patterns, engine, and cache.
 $environment = [
-  'development' => 'dev',
-  'qa'          => 'qa',
-  'staging'     => 'staging',
-  'production'  => 'prod'
+  'development' => [
+    'data' => 'dev',
+    'engine' => 'dev',
+    'patterns' => 'dev',
+    'cache' => 'dev'
+  ],
+  'qa' => [
+    'data' => 'qa',
+    'engine' => 'qa',
+    'patterns' => 'qa',
+    'cache' => 'qa'
+  ],
+  'staging' => [
+    'data' => 'staging',
+    'engine' => 'prod',
+    'patterns' => 'prod',
+    'cache' => 'staging'
+  ],
+  'production' => [
+    'data' => 'prod',
+    'engine' => 'prod',
+    'patterns' => 'prod',
+    'cache' => 'prod'
+  ]
 ];
 
 // Set path globals.
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('SITE_ROOT', __DIR__);
-define('SERVER_ROOT', dirname(__DIR__)); 
+define('SERVER_ROOT', dirname(__DIR__));
 define('SERVER_PATH', str_replace(DOCUMENT_ROOT.'/', '', SERVER_ROOT));
-define('DATA_ROOT', SERVER_ROOT.'/data/'.$environment[ENVIRONMENT]);
-define('PATTERNS_ROOT', SERVER_ROOT.'/patterns/'.$environment[ENVIRONMENT]);
-define('ENGINE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT]);
-define('CACHE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT].'/php/cache');
+define('DATA_ROOT', SERVER_ROOT.'/data/'.$environment[ENVIRONMENT]['data']);
+define('PATTERNS_ROOT', SERVER_ROOT.'/patterns/'.$environment[ENVIRONMENT]['patterns']);
+define('ENGINE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT]['engine']);
+define('CACHE_ROOT', SERVER_ROOT.'/engine/'.$environment[ENVIRONMENT]['cache'].'/php/cache');
 define('SITE_DATA', DATA_ROOT.'/'.SITE);
-  
+
 // Load the templating engine.
 require ENGINE_ROOT."/php/engine.php";
 
